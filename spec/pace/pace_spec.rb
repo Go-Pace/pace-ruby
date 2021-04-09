004c9717124abcee4f324ecf74d0b38bd26c0972
paingha <apaingha@gmail.com>
2021-04-09 01:04:18 -0500

    added github actions

--- spec/pace/pace_spec.rb
+++ spec/pace/pace_spec.rb
@@ -2,9 +2,5 @@ RSpec.describe Pace do
     it "has a version number" do
       expect(Pace::Ruby::VERSION).not_to be nil
     end
-  
-    it "does something useful" do
-      expect(false).to eq(true)
-    end
   end
   
